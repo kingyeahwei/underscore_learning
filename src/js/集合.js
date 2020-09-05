10efc0581d198d802413ff2e82c6906a6737d71b
jinyanwei <kingyeahwei@163.com>
2020-09-06 01:15:29 +0800

    where

--- src/js/集合.js
+++ src/js/集合.js
@@ -22,8 +22,44 @@
 
 // console.log(arr);
 
-var sum = _.reduce([1, 2, 3], function(memo, ele, index, arr) {
-  console.log(memo, ele, index, arr);
-  return memo + ele;
-}, 0)
-console.log(sum);
+// var sum = _.reduce([1, 2, 3], function(memo, ele, index, arr) {
+//   console.log(memo, ele, index, arr);
+//   return memo + ele;
+// }, 0)
+// console.log(sum);
+
+// var list = [
+//   [0, 1],
+//   [2, 3],
+//   [4, 5],
+// ];
+
+// var flat = _.reduceRight(
+//   list,
+//   function (a, b) {
+//     return a.concat(b);
+//   },
+//   []
+// );
+// console.log(flat);
+
+// var even = _.find([1, 2, 3, 4, 5, 6], function(num) {
+//   return num % 2 == 0;
+// }, {one: 1})
+
+// console.log(even);
+
+// var evens = _.filter([1, 2, 3, 4, 5, 6], function(num) {
+//   return num % 2 === 0;
+// }, {one:1})
+
+// console.log(evens);
+
+// var people = [
+//   { name: 'sakshi', hasLong: 'false' },
+//   { name: 'aishwarya', hasLong: 'true' },
+//   { name: 'akansha', hasLong: 'true' },
+//   { name: 'preeti', hasLong: 'true' },
+// ];
+
+// console.log(_.findWhere(people, {hasLong: "true"}));
